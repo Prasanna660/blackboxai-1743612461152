86960aa11f6dd29bcf83e85587d8184efc7d779d
Prasanna660 <115770359+Prasanna660@users.noreply.github.com>
2025-04-12 07:50:50 +0000

    Add arbitrage_trader/arbitrage_trader/urls.py 1744444249962

--- arbitrage_trader/arbitrage_trader/urls.py
+++ arbitrage_trader/arbitrage_trader/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("arbitrage.urls")),
+    path('admin/', admin.site.urls),
+    path('api/', include('arbitrage.urls')),
+    path('api-token-auth/', views.obtain_auth_token),
 ]
